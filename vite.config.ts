23127a7a5369e9938118d6fbe9d33189a7f2fcf8
Thomas Kuschan <mail@thomaskuschan.de>
2025-10-16 19:49:26 +0200

    Adjust config

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,19 @@ import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'url';
 import { defineConfig, loadEnv } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
+// import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
     return defineConfig({
         base: `/ccm/${process.env.VITE_KEY}/`,
-        plugins: [vue(), eslintPlugin({fix: true}), tailwindcss()],
+        plugins: [
+            // nodePolyfills(), // required for @churchtools/styleguide
+            vue(),
+            eslintPlugin({ fix: true }),
+            tailwindcss()
+        ],
         resolve: {
             dedupe: ['vue'],
             alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
